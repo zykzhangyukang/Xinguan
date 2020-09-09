53f4774d6f042a3dfef748a597fede41950b23c6
zykzhangyukang <3053161401@qq.com>
2020-09-09 20:44:27 +0800

    update md5 test

--- src/test/java/com/coderman/MD5Test.java
+++ src/test/java/com/coderman/MD5Test.java
@@ -16,11 +16,12 @@ public class MD5Test {
      */
     @Test
     public void testMd5(){
-        String username="admin";
-        String salt= UUID.randomUUID().toString()+username;
-        String password="123456";
-        String s = MD5Utils.md5Encryption(password, salt);
-        System.out.println("salt="+salt);
-        System.out.println("password="+s);
+        final String username="admin";
+        final String password="123456";
+        String salt= UUID.randomUUID().toString();
+        String realPassword = MD5Utils.md5Encryption(password, salt);
+        System.out.println("用户名:"+username);
+        System.out.println("盐值:"+salt);
+        System.out.println("加密后的密码:"+realPassword);
     }
 }
