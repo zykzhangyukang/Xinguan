be3b043fa66ec04818142227fcd84ccb628ebca2
zykzhangyukang <3053161401@qq.com>
2020-12-17 22:46:16 +0800

    remove xinguan-vue

--- xinguan-vue/src/views/monitor/druid.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <iframe src="https://www:zykhome.club/api/druid/login.html" frameborder="no" border="0" marginwidth="0" marginheight="0"  allowtransparency="yes" style="overflow:hidden;" height="600px;" width="100%"></iframe>
-</template>
